cfba5ec702ccc3dac148fd635b799347ab6bb5d8
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2019-06-22 22:39:17 +0800

    Fixed test for mock
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- handler/mock_test.go
+++ handler/mock_test.go
@@ -53,6 +53,17 @@ func (o *userServiceMock) AccountDetails(token string) (*cip.GetUserOutput, erro
 	return resp.(*cip.GetUserOutput), args.Error(1)
 }
 
+func (o *userServiceMock) UpdateUser(token string, attrs map[string]string) (*cip.UpdateUserAttributesOutput, error) {
+	args := o.Called(token, attrs)
+
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+
+	return resp.(*cip.UpdateUserAttributesOutput), args.Error(1)
+}
+
 func (o *userServiceMock) GetUser(username string) *user.User {
 	args := o.Called(username)
 
